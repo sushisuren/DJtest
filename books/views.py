6ea3118fe4c0bf432f6cb90f2777957af4f28914
sushisuren <sushisuren@163.com>
2016-04-13 17:35:12 +0800

    add books

---
+++ books/views.py
@@ -0,0 +1,28 @@
+#-*- encoding:utf-8 -*-
+
+from django.shortcuts import render,render_to_response,RequestContext
+from django.http import HttpResponse
+from models import User
+# Create your views here.
+
+def reg2(request):
+    p=User(name='lishasha',uid=33)
+    p.save()
+    return render_to_response('reg.html')
+
+
+def reg5(req):
+    if req.method == 'POST':
+        data = req.POST
+        print data
+    return render_to_response('books/reg.html', locals(), context_instance=RequestContext(req))
+
+def reg(request):
+    # if request.method == 'GET':
+    #     return render_to_response('books/reg.html')
+    if request.method == 'POST':
+        data = request.POST
+        print data
+    return render_to_response("reg.html",locals(),context_instance=RequestContext(request))
+        #return HttpResponse("haha")
+
